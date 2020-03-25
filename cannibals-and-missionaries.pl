d75d9f564de099f75fc830ffa1b4dfe6941ce83d
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-03-25 12:32:58 +0200

    Changed state representation to use integers rather than booleans, the new state representation allows for all types of movements

--- cannibals-and-missionaries.pl
+++ cannibals-and-missionaries.pl
@@ -1,71 +1,52 @@
-% [C1, C2, C3, M1, M2, M3, B] Ci denotes i-th Cannibal, Mi denotes i-th missionary, and B denotes Board using which the movement from side to side is possible
+/*
+  State is a list [C, M, Ship]
+  C is the number of cannibals on the right side
+  M is the number of missionaries on the left side
+  Ship is the position of the ship (1 is right, 0 is left).
+*/
 
 
-move([1, C2, C3, M1, M2, M3, 1], [0, C2, C3, M1, M2, M3, 0]).
-move([0, C2, C3, M1, M2, M3, 0], [1, C2, C3, M1, M2, M3, 1]).
+move([C,M,1], [NC, NM, Ship]) :-
+  (
+    (NC is C - 1, NM=M);   % move 1 cannibal to the left
+    (NC is C - 2, NM=M);   % move 2 cannibals to the left
+    (NM is M - 1, NC=C);   % move 1 missionary to the left
+    (NM is M - 2, NC=C);   % move 2 missionaries to the left
+    (NM is M - 1, NC is C - 1) % move 1 cannibal and 1 missionary to the left
+  ),
+  Ship=0. % change the position of the ship to the left
 
-move([C1, 1, C3, M1, M2, M3, 1], [C1, 0, C3, M1, M2, M3, 0]).
-move([C1, 0, C3, M1, M2, M3, 0], [C1, 1, C3, M1, M2, M3, 1]).
+move([C, M, 0], [NC, NM, Ship]) :-
+  (
+    (NC is C + 1, NM=M);  % move 1 cannibal to the right
+    (NC is C + 2, NM=M);   % move 2 cannibals to the right
+    (NM is M + 1, NC=C);   % move 1 missionary to the right
+    (NM is M + 2, NC=C);   % move 2 missionaries to the right
+    (NM is M + 1, NC is C + 1) % move 1 cannibal and 1 missionary to the right
+  ),
+  Ship=1. % change the position of the ship to the right
 
-move([C1, C2, 1, M1, M2, M3, 1], [C1, C2, 0, M1, M2, M3, 0]).
-move([C1, C2, 0, M1, M2, M3, 0], [C1, C2, 1, M1, M2, M3, 1]).
+safe([C, M, _]) :-
+  C >= 0,
+  M >= 0,
+  C < 4,
+  M < 4,
+  LC is 3 - C,
+  LM is 3 - M,
+  (M >= C ; M=0), % (missionaries on the right >= cannibals on the right) OR (there are no missionaries on the right)
+  (LM >= LC ; LM=0). % (missionaries on the left >= cannibals on the left) OR (there are no missionaries on the left side)
 
-move([C1, C2, C3, 1, M2, M3, 1], [C1, C2, C3, 0, M2, M3, 0]).
-move([C1, C2, C3, 0, M2, M3, 0], [C1, C2, C3, 1, M2, M3, 1]).
+play(Movements) :-
+  play([3,3,1], [ [3,3,1] ], Movements).
 
-move([C1, C2, C3, M1, 1, M3, 1], [C1, C2, C3, M1, 0, M3, 0]).
-move([C1, C2, C3, M1, 0, M3, 0], [C1, C2, C3, M1, 1, M3, 1]).
+play(State, _, L) :-
+  State = [0,0,_],
+  L = [].
 
-move([C1, C2, C3, M1, M2, 1, 1], [C1, C2, C3, M1, M2, 0, 0]).
-move([C1, C2, C3, M1, M2, 0, 0], [C1, C2, C3, M1, M2, 1, 1]).
-
-% unsafe([1, 1, 1, 1, 1, 0, _]).
-% unsafe([1, 1, 1, 0, 1, 1, _]).
-% unsafe([1, 1, 1, 1, 0, 1, _]).
-%
-% unsafe([1, _, 1, 1, 0, 0, _]).
-% unsafe([_, 1, 1, 0, 1, 0, _]).
-% unsafe([1, 1, _, 0, 0, 1, _]).
-%
-%
-% unsafe([0, 0, 0, 0, 0, 1, _]).
-% unsafe([0, 0, 0, 1, 0, 0, _]).
-% unsafe([0, 0, 0, 0, 1, 0, _]).
-%
-% unsafe([0, _, 0, 0, 1, 1, _]).
-% unsafe([_, 0, 0, 0, 1, 1, _]).
-
-unsafe(State) :-
-  count_left_cannibals(State, LC),
-  RC is 3-LC,
-
-  delete_cannibals(State, NewState),
-
-  count_left_missionaries(NewState, LM),
-  RM is 3-LM,
-  !,
-  (((RC > RM) , not(RM = 0)) ; ((LC > LM) , not(LM = 0))).
-
-% play(Movements) :-
-%   play([1,1,1,1,1,1,1], [ [[1,1,1,1,1,1,1]] | T ], Movements).
-
-% number of cannibals currently on the left side (Ci = 0)
-count_left_cannibals([_, _, _, _], C) :-
-  C is 0.
-count_left_cannibals([H | T], C) :-
-  count_left_cannibals(T, NewC),
-  (H = 0 -> C is NewC + 1 ; C = NewC).
-
-% number of missionaries currently of the left side (Mi = 0)
-count_left_missionaries([_], C) :-
-  C is 0.
-count_left_missionaries([H | T], C) :-
-  count_left_missionaries(T, NewC),
-  (H = 0 -> C is NewC + 1 ; C = NewC).
-
-
-% removing cannibals from state list
-delete_cannibals([M1,M2,M3,B], NL) :-
-  NL = [M1, M2, M3, B].
-delete_cannibals([_ | T] , NL) :-
-  delete_cannibals(T, NL).
+play(State, Visited, [H | T]) :- % can this be improved to use only 1 list (visited and movements)
+  move(State, NewState),
+  not(member(NewState, Visited)),
+  safe(NewState),
+  append(Visited, [NewState], NewVisited),
+  play(NewState, NewVisited, T),
+  H = NewState.
